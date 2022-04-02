50359be6dd56e2f2ae20b6adeacd7818209be221
TMBR-final <finalbrtm@gmail.com>
2022-04-03 02:31:00 +0300

    changes 2

--- src/app/modules/about/view-history/view-history.component.spec.ts
+++ src/app/modules/about/view-history/view-history.component.spec.ts
@@ -1,9 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { ViewHistoryComponent } from './view-history.component';
-
-
-
-
 
 describe('ViewHistoryComponent', () => {
   let component: ViewHistoryComponent;
--- src/app/modules/converter/currency-select/currency-select.component.spec.ts
+++ src/app/modules/converter/currency-select/currency-select.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ViewHistoryComponent } from './view-history.component';
+import { CurrencySelectComponent } from './currency-select.component';
 
-describe('ViewHistoryComponent', () => {
-  let component: ViewHistoryComponent;
-  let fixture: ComponentFixture<ViewHistoryComponent>;
+describe('CurrencySelectComponent', () => {
+  let component: CurrencySelectComponent;
+  let fixture: ComponentFixture<CurrencySelectComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ViewHistoryComponent ]
+      declarations: [ CurrencySelectComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ViewHistoryComponent);
+    fixture = TestBed.createComponent(CurrencySelectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
